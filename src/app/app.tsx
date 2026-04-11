abcc8504c021c164d8b35bb97f346ca13e8c069e
Guy Oded <guy@somemail.com>
2026-04-11 21:16:43 +0300

    WIP - add antd, basic setup for layout and a bit of text

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,57 @@
+import { Col, Layout, Row, Table, Typography } from "antd";
+import { Content } from "antd/es/layout/layout";
+import "./app.css";
+
 function App() {
-  return <></>;
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Content style={{ padding: "0 48px" }}>
+        <div className="main-container">
+          <h1 className="title">BREAKING THE ENIGMA</h1>
+          <p className="subtitle">
+            Explore how Enigma works, then try to break it yourself
+          </p>
+          <Typography.Title level={2}>Introduction</Typography.Title>
+          <Typography.Paragraph>
+            The enigma machine is a pretty simple but surprisingly hard to crack{" "}
+            <i>substitution cipher</i>. A substitution cipher works on the
+            principle of jumbling up letters. Most likely you've encountered two
+            very simple substitution ciphers both of which are relevant to
+            understanding the enigma.
+          </Typography.Paragraph>
+          <Typography.Title level={2}>Basic Principles</Typography.Title>
+          <Typography.Paragraph>
+            The first of which is the{" "}
+            <Typography.Text strong={true}>Caesar Cipher</Typography.Text>. In
+            this cipher, each letter is mapped to its consecutive letter in the
+            order they appear in the alphabet. In the English alphabet, that
+            would mean:
+            <Table>
+              <Row>Plain
+                <Col>
+                A
+                </Col>
+                <Col>
+                B
+                </Col><Col>
+                C
+                </Col><Col>
+                D
+                </Col><Col>
+                E
+                </Col><Col>
+                F
+                </Col><Col>
+                G
+                </Col>
+              </Row>
+
+            </Table>
+          </Typography.Paragraph>
+        </div>
+      </Content>
+    </Layout>
+  );
 }
 
 export { App };
---
+++ src/components/caeserCipher/caeserCipher.tsx
@@ -0,0 +1,34 @@
+import {  } from "antd";
+type UppercaseChar =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
+
+interface LetterDataType {
+  letter: UppercaseChar;
+}
+
+const rows: TableRowType<LetterDataType>;
